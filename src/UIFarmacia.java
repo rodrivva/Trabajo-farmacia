2e81ed0550454261024f6b967c9640d2549a4965
rodrivva <rdiazquintanar@gmail.com>
2024-02-27 17:32:12 +0100

    Añadida base del trabajo

---
+++ src/UIFarmacia.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+
+public class UIFarmacia extends JFrame{
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton dispensarButton;
+    private JButton verMedicamentosDispensadosButton;
+    private JButton reponerStockButton;
+    private JButton verProspectoButton;
+    private JTextArea textArea1;
+    private JRadioButton verDispensador1RadioButton;
+    private JRadioButton verDispensador2RadioButton;
+    private JRadioButton verDispensador3RadioButton;
+    private JRadioButton verDispensador4RadioButton;
+    private JPanel farmacia;
+
+    public UIFarmacia() {
+        setTitle("Teoria");
+        setSize(400, 300);
+        setDefaultCloseOperation(JInternalFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        setContentPane(farmacia);
+    }
+    public static void main (String[] args){
+        new UIFarmacia();
+    }
+}
