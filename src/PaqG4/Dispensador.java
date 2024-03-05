215bbbf7802daafb6dd7e102d6ba84fbf92b3132
rodrivva <rdiazquintanar@gmail.com>
2024-03-05 22:55:36 +0100

    Avance con las clases

--- src/PaqG4/Dispensador.java
+++ src/PaqG4/Dispensador.java
@@ -1,5 +1,6 @@
 package PaqG4;
 
 public class Dispensador {
+//Falta hacer la matriz de 20x15. Se puede hacer un generador aleatorio de medicamentos que genere un medicamento aleatorio y haga un deep clone de ese medicamento en numero aletorio que entre 0 y 20
 
 }
